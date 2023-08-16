413678961aae130281015422aeff146bf7697378
AryaJulianda <official.aryajulianda@gmail.com>
2023-08-16 21:19:10 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <Router>
+      <Routes>
+          <Route/>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App;
+
