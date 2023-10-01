e03152a3206495e83afa4f93ba646996275eb7c4
maulanazn <Starknntnk99@gmail.com>
2023-10-01 20:47:23 +0700

    error

--- src/pages/editCandidate/WorkExpView.jsx
+++ src/pages/editCandidate/WorkExpView.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
 import { getWorkExpUserId } from '../../redux/actions/bioActions';
 
 export default function WorkExpView() {
   const {id} = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [openEdit, setOpenEdit] = useState(false);
   const {workexp_get} = useSelector(state => state);
@@ -14,12 +15,8 @@ export default function WorkExpView() {
     dispatch(getWorkExpUserId())
   }, [])
 
-  function Edit(id) {
-    if (openEdit) {
-      setOpenEdit(true)
-    }
-
-    return
+  function Edit() {
+    setOpenEdit(openEdit => !openEdit)
   }
 
   return (
@@ -29,8 +26,17 @@ export default function WorkExpView() {
           return (
             <div key={index}>
               <button onClick={Edit}>cek</button>
-              <img src={item.work_experience_photo} alt={item.user_name}/>
-              <h1>{item.position}</h1>
+              {
+                openEdit ? 
+                <div>
+                  <img src={item.work_experience_photo} alt={item.user_name}/>
+                  <h1>{item.position}</h1>
+                </div> :
+                <form encType='multipart/form-data'>
+                  <input type="text" placeholder='Position' defaultValue={item.position}/>
+                  <button type="submit">Update</button>
+                </form>
+              }
             </div>
           )
         })
